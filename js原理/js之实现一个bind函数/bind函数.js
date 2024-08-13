ed9837ea27972fe1050276e09beddeeb075e0f2d
Administrator <caikaijie@zhizukj.com>
2024-08-13 20:27:39 +0800

    bind函数和new实现过程完善

--- js原理/js之实现一个bind函数/bind函数.js
+++ js原理/js之实现一个bind函数/bind函数.js
@@ -1,6 +1,6 @@
 if (!Function.prototype.bind) {
     Function.prototype.bind = function(context) {
-        var self = this; // 保存原函数
+        var self = this; // 保存原函数greet
         console.log('self', self)
         return function() {
             // 返回一个新函数，当被调用时，这个新函数将执行原函数，并传递所有参数
@@ -17,4 +17,5 @@ var obj = {
     name: 'John'
 };
 var boundGreet = greet.bind(obj);
+
 console.log(boundGreet('Hello')); // 输出: John says Hello
